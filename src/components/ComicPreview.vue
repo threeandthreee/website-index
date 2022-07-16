19a72061b73b42a75d580336f3fb36a3281af6a5
Alex Nordstrom <alnordst@hey.com>
2022-07-16 17:03:16 -0400

    fix comic click

--- src/components/ComicPreview.vue
+++ src/components/ComicPreview.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-iframe(src="https://3and3.the-comic.org/comics" :width="width" :height="height" scrolling="no" style="border:0")
+iframe(src="https://3and3.the-comic.org/comics" :width="width" :height="height" scrolling="no" style="border:0;pointer-events:none;touch-action:none")
 </template>
 
 <script>
